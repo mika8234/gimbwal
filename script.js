f38da8b039507c406cabd4186834e243c71e4e8f
mika8234 <kingbimbo116@gmail.com>
2025-02-24 19:24:07 +0800

    script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ window.addEventListener('load', () => {
 const messages = [
     {
         text: "hmm",
-        image: "https://pin.it/24Oi0XON1"
+        image: "https://i.pinimg.com/736x/1e/7c/6f/1e7c6ff7a0c26f7eaefbce683656193d.jpg"
     },
     {
         text: "hmmm",
